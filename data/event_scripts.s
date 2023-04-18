275576c3e1b002b587fe6f25d904e47a758a6e9d
HunarPG <92902941+HunarPG@users.noreply.github.com>
2023-04-18 12:34:10 +0530

    Cleanup

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -570,6 +570,7 @@ gStdScripts_End::
 	.include "data/maps/Route119_WeatherInstitute_2F/scripts.inc"
 	.include "data/maps/Route119_House/scripts.inc"
 	.include "data/maps/Route124_DivingTreasureHuntersHouse/scripts.inc"
+
 	.include "data/scripts/std_msgbox.inc"
 	.include "data/scripts/trainer_battle.inc"
 	.include "data/scripts/new_game.inc"
